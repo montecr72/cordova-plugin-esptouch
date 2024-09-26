7e044e02183e267def5f54965c8fdb3f643e214e
montecr72 <107093893+montecr72@users.noreply.github.com>
2024-09-26 17:49:07 +0300

    Update esptouch2.js

--- www/esptouch2.js
+++ www/esptouch2.js
@@ -1,7 +1,7 @@
 var exec = require('cordova/exec');
 
 module.exports = {
-  start: function (apSsid, apPassword, customData, successCallback, failCallback) {
+  start: function (apSsid,apBssid, apPassword, customData,aesKey, successCallback, failCallback) {
     exec(successCallback, failCallback, "esptouch2", "start", [apSsid,apBssid, apPassword, customData,aesKey]);
   },
   stop: function (successCallback, failCallback) {
