4dee5ff24f74e7285c2cc9c5d6917722466050b9
montecr72 <107093893+montecr72@users.noreply.github.com>
2024-10-15 01:18:00 +0300

    Update wifi.m

--- src/ios/wifi.m
+++ src/ios/wifi.m
@@ -1,16 +1,42 @@
 #import "wifi.h"
+#import <NetworkExtension/NetworkExtension.h>
+#import <CoreLocation/CoreLocation.h>
 
 @implementation wifi
 
 - (void) getConnectedInfo:(CDVInvokedUrlCommand *)command{
-    NSDictionary *wifiDic = @{
-        @"ssid":ESPTools.getCurrentWiFiSsid,
-        @"bssid":ESPTools.getCurrentBSSID,
-        @"state":@"Connected"
-    };
+    CLLocationManager *manager;
+    manager = [[CLLocationManager alloc] init];
+    [manager requestWhenInUseAuthorization];
+    NEHotspotConfigurationManager* hs_manager = [NEHotspotConfigurationManager sharedManager];
+    
+    [NEHotspotNetwork fetchCurrentWithCompletionHandler:^(NEHotspotNetwork * _Nullable currentNetwork) {
+        NSDictionary *wifiDic;
+        if(currentNetwork.SSID != nil){
+            NSLog(@"current :%@", ESPTools.getCurrentWiFiSsid);
+            wifiDic = @{
+                @"ssid":currentNetwork.SSID,
+                @"bssid":currentNetwork.BSSID,
+                @"state":@"Connected"
+            };
+        }else if(ESPTools.getCurrentWiFiSsid != nil){
+            NSLog(@"current :%@", ESPTools.getCurrentWiFiSsid);
+            wifiDic = @{
+                @"ssid":ESPTools.getCurrentWiFiSsid,
+                @"bssid":ESPTools.getCurrentBSSID,
+                @"state":@"Connected"
+            };
+        }else{
+            wifiDic = @{
+                @"state":@"bad request"
+            };
+        }
+        
+        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:wifiDic];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
 
-    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:wifiDic];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+  
 }
 
 @end
