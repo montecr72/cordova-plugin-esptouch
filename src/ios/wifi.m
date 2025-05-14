52fe6e38d649b217fd7a86a113cfce53679f9a73
montecr72 <107093893+montecr72@users.noreply.github.com>
2025-05-14 15:25:49 +0300

    Update wifi.m

--- src/ios/wifi.m
+++ src/ios/wifi.m
@@ -8,34 +8,18 @@
     CLLocationManager *manager;
     manager = [[CLLocationManager alloc] init];
     [manager requestWhenInUseAuthorization];
-    NEHotspotConfigurationManager* hs_manager = [NEHotspotConfigurationManager sharedManager];
     
-    [NEHotspotNetwork fetchCurrentWithCompletionHandler:^(NEHotspotNetwork * _Nullable currentNetwork) {
-        NSDictionary *wifiDic;
-        if(currentNetwork.SSID != nil){
-            NSLog(@"current :%@", ESPTools.getCurrentWiFiSsid);
-            wifiDic = @{
-                @"ssid":currentNetwork.SSID,
-                @"bssid":currentNetwork.BSSID,
-                @"state":@"Connected"
-            };
-        }else if(ESPTools.getCurrentWiFiSsid != nil){
-            NSLog(@"current :%@", ESPTools.getCurrentWiFiSsid);
-            wifiDic = @{
-                @"ssid":ESPTools.getCurrentWiFiSsid,
-                @"bssid":ESPTools.getCurrentBSSID,
-                @"state":@"Connected"
-            };
-        }else{
-            wifiDic = @{
-                @"state":@"bad request"
-            };
-        }
-        
-        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:wifiDic];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    }];
+    NSDictionary *wifiDic;
 
+    NSLog(@"current :%@", ESPTools.getCurrentWiFiSsid);
+    wifiDic = @{
+        @"ssid":ESPTools.getCurrentWiFiSsid,
+        @"bssid":ESPTools.getCurrentBSSID,
+        @"state":@"Connected"
+    };
+    
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:wifiDic];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
   
 }
 
