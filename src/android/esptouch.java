1cf3e2c30188ba78625c78ad4199c9df733164bb
montecr72 <107093893+montecr72@users.noreply.github.com>
2025-05-22 22:04:26 +0300

    Update esptouch.java

--- src/android/esptouch.java
+++ src/android/esptouch.java
@@ -76,7 +76,7 @@ public class esptouch extends CordovaPlugin {
         //final byte[] deviceCountData = strToByteArray("1");
         //final byte[] broadcastData = strToByteArray("1");
         final byte[] deviceCountData = strToByteArray(args.getString(3));
-        final byte[] broadcastData = strToByteArray(args.getString(3));
+        final byte[] broadcastData = strToByteArray(args.getString(4));
         taskResultCount = deviceCountData.length == 0 ? -1 : Integer.parseInt(new String(deviceCountData));
         mEsptouchTask = new EsptouchTask(apSsid, apBssid, apPassword, cordova.getActivity());
         mEsptouchTask.setPackageBroadcast(broadcastData[0] == 49);
